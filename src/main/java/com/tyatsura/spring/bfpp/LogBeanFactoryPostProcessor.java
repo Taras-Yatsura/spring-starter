27a4439ac77dd0a5888bfa6a07691e5d3c15e73a
Taras Yatsura <jatsura.taras@gmail.com>
2023-01-27 16:26:23 +0200

    Annotation based bean configuration: - @Component that replace xml bean definition and allow use constructor

--- src/main/java/com/tyatsura/spring/bfpp/LogBeanFactoryPostProcessor.java
+++ src/main/java/com/tyatsura/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.PriorityOrdered;
+import org.springframework.stereotype.Component;
 
 //PriorityOrdered calling before Ordered
 //will be called first as here set HIGHEST_PRECEDENCE and in PropertyResourceConfigurer is used LOWEST_PRECEDENCE
 //than will be called FirstVerifyBeanFactoryPostProcessor and after VerifyBeanFactoryPostProcessor
 // (less order - earlier call)
+@Component
 public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
